7c4883b527f9eb646237f4d0fd98924a006c2d26
Joe LeBeau <joseph.p.lebeau@gmail.com>
2017-03-21 13:39:00 -0500

    add js

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("The script is running!");
+
+function addRedBorder(event) {
+    event.target.style.border = "3px solid red";
+}
+
+var info = document.getElementsByClass("info");
+photo.addEventListener("click", addRedBorder);
+
